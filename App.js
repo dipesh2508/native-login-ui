08ad505b8c3e0751bf5e276c25727a4dadcb4d20
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-04-10 12:08:04 +0530

    feat:developed the ui

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 justify-center items-center bg-white'>
+      <StatusBar style='auto' />
+      <Text className='text-center mt-3 text-4xl text-slate-800 font-semibold'>
+        Login
+      </Text>
+      <View className='mt-5 mx-5'>
+        <View>
+          <Text className='text-gray-600'>EMAIL:</Text>
+          <TextInput
+            placeholder='Enter Email...'
+            className='border border-dotted p-2 text-gray-500 border-sky-400 mt-1 rounded-md'
+          />
+        </View>
+        <View className='mt-3'>
+          <Text className='text-gray-600'>PASSWORD:</Text>
+          <TextInput
+            secureTextEntry
+            placeholder='Enter Password...'
+            className='border text-gray-500 border-dotted p-2 border-sky-400 mt-1 rounded-md'
+          />
+        </View>
+
+        <TouchableOpacity className='bg-teal-300 p-3 mt-8 rounded-md'>
+          <Text className='text-center text-base text-white'>Login</Text>
+        </TouchableOpacity>
+
+        
+        <View className='mt-6 flex-row justify-center'>
+          <Text className=''>New to the application? </Text>
+          <TouchableOpacity>
+            <Text className='text-sky-500'>Create an Account</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Readme 
+
+## Description
+
+This is a simple project to develop the UI of the login page of an application using react native.
+
+
+### Reasons to Develop this project
+
+- To learn the basics of react native.
+- To learn how to create a login page using react native.
+- To learn how to use the react native components.
+
+
+```Note: This project is only for learning purposes.```
